03c5b7f637cbbbcec9c1d7ecd2d4c51cc0ef8d3d
otter1614 <als2ek1614@gmail.com>
2025-05-12 17:34:21 +0900

    초기화

---
+++ src/Test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+import "./Test.css"
+
+const TestContainer = styled.div`
+  width: 500px;
+  background-color: gray;
+`;
+
+const Ptest = styled.p`
+  font-weight: bold;
+`;
+
+function Test() {
+  return (
+   
+      <TestContainer>
+        <Ptest>안녕하세요 Ptest입니다</Ptest>
+        <div className="asd">안녕하세요 asd 입니다다</div>
+      </TestContainer>
+  );
+}
+
+export default Test;
